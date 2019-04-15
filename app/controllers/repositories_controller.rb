bef391d95cb7c4a8c98904bdc9250a5f7ef0f0c7
Mika88 <mikaspivak@gmail.com>
2019-04-15 20:57:58 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,7 +2,6 @@ class RepositoriesController < ApplicationController
 
   def index
     response = Faraday.get("https://github.com/user")
-  end
 
     body = JSON.parse(response.body)
     @user = body["login"]
